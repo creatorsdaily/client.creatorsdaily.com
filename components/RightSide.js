afd3727c1f268cf6be6e827482f13b0fd50e7c20
Tengfei <tengfei@tengfei.fun>
2019-10-20 15:19:29 +0800

    add wechat

--- components/RightSide.js
+++ components/RightSide.js
@@ -1,4 +1,4 @@
-import { Affix } from 'antd'
+import { Affix, Button, Modal } from 'antd'
 import styled from 'styled-components'
 import noop from 'lodash/noop'
 import Box from './Box'
@@ -11,18 +11,51 @@ img {
 }
 `
 
+const SmallButton = styled(Button)`
+  font-size: 12px;
+  color: #6C6C6C;
+`
+
 const Title = styled.h3`
   font-size: 14px;
   text-align: center;
 `
 
+const Buttons = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 24px;
+`
+
+const WeChatQRCode = styled.img`
+  width: 100%;
+`
+
 export default ({ onQuestion = noop, onProduct = noop }) => {
+  const handleWeChat = () => {
+    Modal.info({
+      okText: '关闭',
+      className: 'preview-modal',
+      icon: null,
+      maskClosable: true,
+      autoFocusButton: null,
+      width: 500,
+      content: (
+        <WeChatQRCode src='/wechat.jpg' />
+      )
+    })
+  }
   return (
     <Affix offsetTop={24}>
       <StyledBox>
         <Title>微信公众号</Title>
         <WeChatOfficialAccount title='微信关注：一群创造者' />
       </StyledBox>
+      <Buttons>
+        <SmallButton size='small' type='link' onClick={handleWeChat}>
+          微信
+        </SmallButton>
+      </Buttons>
     </Affix>
   )
 }
