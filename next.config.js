0e26728859328ed9a569757038f548d0dc6d0be0
Tengfei <tengfei@guanghe.tv>
2019-12-02 23:13:38 +0800

    fix: 解决首页不更新问题

--- next.config.js
+++ next.config.js
@@ -54,24 +54,24 @@ module.exports = withPlugins([
     dontAutoRegisterSw: true,
     transformManifest: manifest => ['/'].concat(manifest),
     workboxOpts: {
-      swDest: path.join(__dirname, 'public/service-worker.js'),
-      runtimeCaching: [
-        {
-          urlPattern: /^https?.*/,
-          handler: 'NetworkFirst',
-          options: {
-            cacheName: 'https-calls',
-            networkTimeoutSeconds: 15,
-            expiration: {
-              maxEntries: 150,
-              maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
-            },
-            cacheableResponse: {
-              statuses: [0, 200]
-            }
-          }
-        }
-      ]
+      swDest: path.join(__dirname, 'public/service-worker.js')
+      // runtimeCaching: [
+      //   {
+      //     urlPattern: /^https?.*/,
+      //     handler: 'NetworkFirst',
+      //     options: {
+      //       cacheName: 'https-calls',
+      //       networkTimeoutSeconds: 15,
+      //       expiration: {
+      //         maxEntries: 150,
+      //         maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
+      //       },
+      //       cacheableResponse: {
+      //         statuses: [0, 200]
+      //       }
+      //     }
+      //   }
+      // ]
     }
   }],
   [withLess, {
